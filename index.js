862fdfb953498dea185341ec2bad7105ab3ed5c9
thomas <thomas@flexio.fr>
2018-07-12 10:23:31 +0200

    update import

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import {Dispatcher} from 'src/Dispatcher'
+import {Dispatcher} from './src/Dispatcher'
 import {
   Store,
   INIT as STORE_INIT,
   CHANGED
-} from 'src/storeBases/Store'
-import {EventHandler} from 'src/EventHandler'
-import {EventOrderedHandler} from 'src/EventOrderedHandler'
-import {Action} from 'src/Action'
+} from './src/storeBases/Store'
+import {EventHandler} from './src/EventHandler'
+import {EventOrderedHandler} from './src/EventOrderedHandler'
+import {Action} from './src/Action'
 import {
   View,
   ATTRIBUTE_NODEREF,
@@ -18,25 +18,25 @@ import {
   MOUNTED, STATE_CHANGE,
   STATE_CHANGED,
   STORE_CHANGED
-} from 'src/View'
-import {MapOfState} from 'src/MapOfState'
-import {ViewContainer, ViewContainerParameters} from 'src/ViewContainer'
-import {HotBalloonApplication} from 'src/HotBalloonApplication'
-import {ApplicationContextMixin} from 'src/mixins/ApplicationContextMixin'
-import {ComponentContextMixin} from 'src/mixins/ComponentContextMixin'
-import {PrivateStateMixin} from 'src/mixins/PrivateStateMixin'
-import {Component} from 'src/Component'
+} from './src/View'
+import {MapOfState} from './src/MapOfState'
+import {ViewContainer, ViewContainerParameters} from './src/ViewContainer'
+import {HotBalloonApplication} from './src/HotBalloonApplication'
+import {ApplicationContextMixin} from './src/mixins/ApplicationContextMixin'
+import {ComponentContextMixin} from './src/mixins/ComponentContextMixin'
+import {PrivateStateMixin} from './src/mixins/PrivateStateMixin'
+import {Component} from './src/Component'
 
-import {html} from 'src/HotballoonElement/CreateHotBalloonElement'
-import {HotballoonElementParams} from 'src/HotballoonElement/HotballoonElementParams'
+import {html} from './src/HotballoonElement/CreateHotBalloonElement'
+import {HotballoonElementParams} from './src/HotballoonElement/HotballoonElementParams'
 import {
   HotBalloonAttributeHandler,
   select as selectAttributeHandler
-} from 'src/HotballoonElement/HotBalloonAttributeHandler'
+} from './src/HotballoonElement/HotBalloonAttributeHandler'
 
-import {State} from 'src/storeBases/State'
-import {Storage} from 'src/storeBases/Storage'
-import {StoreModel} from 'src/storeBases/StoreModel'
+import {State} from './src/storeBases/State'
+import {Storage} from './src/storeBases/Storage'
+import {StoreModel} from './src/storeBases/StoreModel'
 
 export const hotballoonElement = {
   html,
