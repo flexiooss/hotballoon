4cbb20f1f460135849d6fc8a0f5f2dd6b7244e6a
tomacht <thomas@flexio.fr>
2019-09-23 17:12:46 +0200

    refactor actionBuilder

--- src/js/Action/ActionDispatcherBuilder.js
+++ src/js/Action/ActionDispatcherBuilder.js
@@ -8,55 +8,87 @@ import {assertType} from '@flexio-oss/assert'
  * @template TYPE, TYPE_BUILDER
  */
 export class ActionDispatcherBuilder {
+  constructor() {
+    /**
+     *
+     * @type {?{Dispatcher}}
+     * @private
+     */
+    this.__dispatcher = null
+    this.__type = null
+    this.__payloadBuilder = null
+    this.__validator = () => true
+    this.__defaultChecker = v => v
+  }
+
   /**
    *
-   * @param {PublicActionDispatcherConfig<TYPE, TYPE_BUILDER>} publicActionParams
+   * @param {Dispatcher} dispatcher
+   * @return {ActionDispatcherBuilder}
+   */
+  dispatcher(dispatcher) {
+    this.__dispatcher = dispatcher
+    return this
+  }
+
+  /**
+   *
+   * @param {TYPE.} type
+   * @return {ActionDispatcherBuilder}
+   */
+  type(type) {
+    this.__type = type
+    return this
+  }
+
+  /**
+   *
+   * @param {TYPE_BUILDER.} payloadBuilder
+   * @return {ActionDispatcherBuilder}
+   */
+  payloadBuilder(payloadBuilder) {
+    this.__payloadBuilder = payloadBuilder
+    return this
+  }
+
+  /**
+   *
+   * @param {ActionTypeConfig~defaultCheckerClb<TYPE>} [defaultChecker=data=>data]
+   * @return {ActionDispatcherBuilder}
+   */
+  defaultChecker(defaultChecker) {
+    this.__defaultChecker = defaultChecker
+    return this
+  }
+
+  /**
+   *
+   * @param {ActionTypeConfig~validatorClb<TYPE>} [validator=data=>true]
+   * @return {ActionDispatcherBuilder}
+   */
+  validator(validator) {
+    this.__validator = validator
+    return this
+  }
+
+  /**
+   *
    * @return {ActionDispatcher<TYPE, TYPE_BUILDER>}
    */
-  static build(publicActionParams) {
-
-    assertType(
-      publicActionParams instanceof PublicActionDispatcherConfig,
-      'hotballoon:ActionDispatcherBuilder:build: `publicActionParams` should be an instance of PublicActionDispatcherConfig'
-    )
+  build() {
 
     return new ActionDispatcher(
       new ActionDispatcherConfig(
-        UID(publicActionParams.params.name + '_'),
-        publicActionParams.params,
-        publicActionParams.dispatcher
+        UID(this.__type.name + '_'),
+        new ActionTypeConfig(
+          this.__type,
+          this.__payloadBuilder,
+          this.__defaultChecker,
+          this.__validator
+        ),
+        this.__dispatcher
       )
     )
   }
-}
 
-/**
- * @template TYPE, TYPE_BUILDER
- */
-export class PublicActionDispatcherConfig {
-  /**
-   *
-   * @param {ActionTypeConfig<TYPE, TYPE_BUILDER>} actionTypeConfig
-   * @param {Dispatcher} dispatcher
-   */
-  constructor(actionTypeConfig, dispatcher) {
-    this._params = actionTypeConfig
-    this._dispatcher = dispatcher
-  }
-
-  /**
-   *
-   * @return {ActionTypeConfig<TYPE, TYPE_BUILDER>}
-   */
-  get params() {
-    return this._params
-  }
-
-  /**
-   *
-   * @return {Dispatcher}
-   */
-  get dispatcher() {
-    return this._dispatcher
-  }
 }
