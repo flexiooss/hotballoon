1d25b72855789476b005c4ba261ce1e88c08b555
Basheitan <antoine@flexio.fr>
2020-01-15 12:06:52 +0100

    add store type builder to jsDoc

--- src/js/Store/ProxyStoreBuilder.js
+++ src/js/Store/ProxyStoreBuilder.js
@@ -7,7 +7,7 @@ import {ProxyStoreConfig} from './ProxyStoreConfig'
 
 /**
  *
- * @template  STORE_TYPE, TYPE, TYPE_BUILDER
+ * @template  STORE_TYPE, STORE_TYPE_BUILDER,  TYPE, TYPE_BUILDER
  */
 export class ProxyStoreBuilder {
   constructor() {
@@ -48,7 +48,7 @@ export class ProxyStoreBuilder {
 
   /**
    *
-   * @param {StoreInterface<STORE_TYPE>} store
+   * @param {StoreInterface<STORE_TYPE, STORE_TYPE_BUILDER>} store
    * @return {ProxyStoreBuilder}
    */
   store(store) {
