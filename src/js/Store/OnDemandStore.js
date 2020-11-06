3e2eedff322cd920a4b86bb5d3e1ed5dbfc960ad
tomacht <thomas@flexio.fr>
2020-11-06 17:19:27 +0100

    add unMount

---
+++ src/js/Store/OnDemandStore.js
@@ -0,0 +1,37 @@
+import {StoreInterface} from './StoreInterface'
+import {Store} from './Store'
+import {_storage} from './StoreBase'
+
+/**
+ * @template TYPE, TYPE_BUILDER
+ * @extends {Store<TYPE, TYPE_BUILDER>}
+ * @implements {StoreInterface<TYPE, TYPE_BUILDER>}
+ * @implements {GenericType<TYPE>}
+ * @implements  {HasTagClassNameInterface}
+ */
+export class OnDemandStore extends Store {
+  /**
+   * @type {function(state: TYPE ):TYPE}
+   */
+  #mapper
+
+  /**
+   * @constructor
+   * @param {OnDemandStoreConfig<TYPE, TYPE_BUILDER>} storeConfig
+   */
+  constructor(storeConfig) {
+    super(storeConfig)
+    this.#mapper = storeConfig.mapper()
+  }
+
+  /**
+   * @returns {StoreState<TYPE>} state frozen
+   */
+  state() {
+    return this[_storage].set(
+      this.ID(),
+      this.validateDataStore(this.#mapper.call(null, super.state())
+      )
+    )
+  }
+}
