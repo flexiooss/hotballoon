5200b705b9a08e4c0602d0cb697e2c06e1a77ad8
tomacht <thomas@flexio.fr>
2023-12-08 11:26:56 +0100

    typecheck

--- src/js/Types/TypeCheck.js
+++ src/js/Types/TypeCheck.js
@@ -13,7 +13,7 @@ import {
   CLASS_TAG_NAME_ACTION_SUBSCRIBER,
   testClassTagName
 } from './HasTagClassNameInterface.js'
-import {assertType, isNull} from '@flexio-oss/js-commons-bundle/assert/index.js'
+import {assertType, formatType, isNull} from '@flexio-oss/js-commons-bundle/assert/index.js'
 import {ElementDescription} from '../HotballoonNodeElement/ElementDescription.js'
 import {RemovedException} from "../Exception/RemovedException.js";
 import {implementsHBComponent} from "../Component/Component.js";
@@ -78,129 +78,138 @@ class TypeCheck {
   }
 
   /**
-   * @param {Object} inst
+   * @param {ActionDispatcher<*>} inst
+   * @param {?Class} [typeOf=null]
    * @return {boolean}
    */
-  static isActionDispatcher(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_ACTION_DISPATCHER)
+  static isActionDispatcher(inst, typeOf = null) {
+    return testClassTagName(inst, CLASS_TAG_NAME_ACTION_DISPATCHER) && (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
   }
 
   /**
-   * @param {ActionDispatcher} inst
-   * @param {Class} typeOf
-   * @param {?string} stringName
-   * @return {ActionDispatcher}
+   * @param {ActionDispatcher<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {ActionDispatcher<*>}
    * @throws {TypeError}
    */
   static assertIsActionDispatcher(inst, typeOf = null, stringName = null) {
-    assertType(TypeCheck.isActionDispatcher(inst),
-      'TypeCheck:assertIsActionDispatcher: `inst` argument should be an ActionDispatcher'
-    )
-    if (!isNull(typeOf)) {
-      assertType(inst.isTypeOf(typeOf),
-        `TypeCheck:assertIsActionDispatcher: \`inst\` argument should be a ${!isNull(stringName) ? stringName : typeOf.constructor.name}`
-      )
-    }
-    return inst
-  }
-
-  /**
-   * @param {Object} inst
-   * @return {boolean}
-   */
-  static isActionSubscriber(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_ACTION_SUBSCRIBER) || TypeCheck.isActionDispatcher(inst)
-  }
-
-  /**
-   * @param {ActionSubscriber} inst
-   * @return {ActionSubscriber}
-   * @throws {TypeError}
-   */
-  static assertIsActionSubscriber(inst) {
-    assertType(TypeCheck.isActionSubscriber(inst),
-      'TypeCheck:assertIsActionDispatcher: `inst` argument should be an ActionSubscriber'
+    assertType(
+      TypeCheck.isActionDispatcher(inst, typeOf),
+      () => `should be an ActionDispatcher<${stringName ?? '*'}, given:${formatType(inst)}>`
     )
     return inst
   }
 
   /**
-   * @param {Store} inst
+   * @param {Object} inst
+   * @param {?Class} [typeOf=null]
    * @return {boolean}
    */
-  static isStore(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_STORE)
+  static isActionSubscriber(inst, typeOf = null) {
+    return (testClassTagName(inst, CLASS_TAG_NAME_ACTION_SUBSCRIBER) || TypeCheck.isActionDispatcher(inst)) && (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
   }
 
   /**
-   * @param {Store} inst
-   * @return {Store}
+   * @param {ActionSubscriber<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {ActionSubscriber<*>}
    * @throws {TypeError}
    */
-  static assertIsStore(inst) {
-    assertType(TypeCheck.isStore(inst), '`inst` should be `Store`')
+  static assertIsActionSubscriber(inst, typeOf = null, stringName = null) {
+    assertType(
+      TypeCheck.isActionSubscriber(inst, typeOf),
+      () => `should be an ActionSubscriber<${stringName ?? '*'}, given:${formatType(inst)}>`
+    )
+    return inst
+  }
+
+  /**
+   * @param {Store<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @return {boolean}
+   */
+  static isStore(inst, typeOf = null) {
+    return testClassTagName(inst, CLASS_TAG_NAME_STORE) && (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
+  }
+
+  /**
+   * @param {Store<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {Store<*>}
+   * @throws {TypeError}
+   */
+  static assertIsStore(inst, typeOf = null, stringName = null) {
+    assertType(TypeCheck.isStore(inst, typeOf),
+      () => `should be an Store<${stringName ?? '*'}, given:${formatType(inst)}>`)
     return inst
   }
 
   /**
    * @param {Object} inst
+   * @param {?Class} [typeOf=null]
    * @return {boolean}
    */
-  static isProxyStore(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_PROXYSTORE)
+  static isProxyStore(inst, typeOf = null) {
+    return testClassTagName(inst, CLASS_TAG_NAME_PROXYSTORE) && (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
   }
 
   /**
-   * @param {ProxyStore} inst
-   * @return {ProxyStore}
+   * @param {ProxyStore<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {ProxyStore<*>}
    * @throws {TypeError}
    */
-  static assertIsProxyStore(inst) {
-    assertType(TypeCheck.isProxyStore(inst), '`inst` should be `ProxyStore`')
+  static assertIsProxyStore(inst, typeOf = null, stringName = null) {
+    assertType(TypeCheck.isProxyStore(inst, typeOf),
+      () => `should be an ProxyStore<${stringName ?? '*'}, given:${formatType(inst)}>`)
     return inst
   }
 
   /**
-   * @param {PublicStoreHandler} inst
+   * @param {PublicStoreHandler<*>} inst
+   * @param {?Class} [typeOf=null]
    * @return {boolean}
    */
-  static isPublicStoreHandler(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_PUBLIC_STORE_HANDLER)
+  static isPublicStoreHandler(inst, typeOf = null) {
+    return testClassTagName(inst, CLASS_TAG_NAME_PUBLIC_STORE_HANDLER) && (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
   }
 
   /**
-   * @param {PublicStoreHandler} inst
-   * @param {Class} typeOf
-   * @param {?string} stringName
-   * @return {PublicStoreHandler}
+   * @param {PublicStoreHandler<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {PublicStoreHandler<*>}
    * @throws {TypeError}
    */
   static assertIsPublicStoreHandler(inst, typeOf = null, stringName = null) {
-    assertType(TypeCheck.isPublicStoreHandler(inst), '`inst` should be `PublicStoreHandler`')
-    if (!isNull(typeOf)) {
-      assertType(inst.isTypeOf(typeOf),
-        `TypeCheck:assertIsPublicStoreHandler: \`inst\` argument should be a ${!isNull(stringName) ? stringName : typeOf.constructor.name}`
-      )
-    }
+    assertType(TypeCheck.isPublicStoreHandler(inst,typeOf), () => `should be an PublicStoreHandler<${stringName ?? '*'}, given:${formatType(inst)}>`)
+
     return inst
   }
 
   /**
-   * @param {Object} inst
+   * @param {StoreBase<*>} inst
+   * @param {?Class} [typeOf=null]
    * @return {boolean}
    */
-  static isStoreBase(inst) {
-    return testClassTagName(inst, CLASS_TAG_NAME_STORE) || testClassTagName(inst, CLASS_TAG_NAME_PROXYSTORE) || testClassTagName(inst, CLASS_TAG_NAME_PUBLIC_STORE_HANDLER)
+  static isStoreBase(inst,typeOf = null) {
+    return (testClassTagName(inst, CLASS_TAG_NAME_STORE) || testClassTagName(inst, CLASS_TAG_NAME_PROXYSTORE) || testClassTagName(inst, CLASS_TAG_NAME_PUBLIC_STORE_HANDLER)) &&  (!isNull(typeOf) ? inst.isTypeOf(typeOf) : true)
   }
 
   /**
    *
-   * @param {StoreInterface} inst
-   * @return {StoreInterface}
+   * @param {StoreBase<*>} inst
+   * @param {?Class} [typeOf=null]
+   * @param {?string} [stringName=null]
+   * @return {StoreBase<*>}
    */
-  static assertStoreBase(inst) {
-    assertType(TypeCheck.isStoreBase(inst),
-      'TypeCheck:assertStoreBase: `inst` argument should be an StoreInterface'
+  static assertStoreBase(inst, typeOf = null, stringName = null) {
+    assertType(TypeCheck.isStoreBase(inst,typeOf ),
+      () => `should be an StoreBase<${stringName ?? '*'}, given:${formatType(inst)}>`
     )
     return inst
   }
@@ -323,7 +332,7 @@ class TypeCheck {
    * @param {ListenedEvent} inst
    * @return {boolean}
    */
-  static implementsListenedEvent(inst){
+  static implementsListenedEvent(inst) {
     return implementsListenedEvent(inst)
   }
 
