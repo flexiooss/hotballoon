999f6b8a26fdd1a65114aa8e91f2ee3efaea4ecf
AntoineBcht <duckzed@gmail.com>
2024-01-12 08:59:46 +0100

    add throttle test dev

---
+++ src/test/dev/store-throttled.js
@@ -0,0 +1,47 @@
+import {InMemoryStoreBuilder} from '../../js/Store/InMemoryStoreBuilder.js'
+import {FakeValueObject, FakeValueObjectBuilder} from '../FakeValueObject.js'
+import {ProxyStoreListenerThrottledBuilder} from '../../js/Store/ProxyStoreListenerThrottled.js'
+
+const store = new InMemoryStoreBuilder()
+  .type(FakeValueObject)
+  .initialData(
+    new FakeValueObjectBuilder()
+      .a(1)
+      .b(10)
+      .build()
+  )
+  .build()
+
+let invoked = 0
+let state = null
+/**
+ * @type {ProxyStoreListenerThrottled}
+ */
+const proxyStore = new ProxyStoreListenerThrottledBuilder()
+  .store(store)
+  .timeToThrottle(300)
+  .build()
+
+proxyStore.listenChanged(
+  b => b.callback((p) => {
+
+    invoked++
+    state = p.data()
+  })
+    .build()
+)
+
+document.body.innerHTML = '<button id="button">click</button>'
+const button = document.getElementById('button')
+button.addEventListener('click', () => {
+  store.set(
+    new FakeValueObjectBuilder()
+      .a(1)
+      .b(20)
+      .build())
+  store.set(
+    new FakeValueObjectBuilder()
+      .a(5)
+      .b(20)
+      .build())
+})
